af89fe3607349ebabd349d1f5942041a021e9e9a
Sergi <sergi_tanya@mail.ru>
2021-07-11 18:58:04 +0200

    login page, authentication service

--- angular-blog/src/app/admin/admin.module.ts
+++ angular-blog/src/app/admin/admin.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { AdminLayoutComponent } from './shared/component/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
@@ -26,5 +32,6 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     ])
   ],
   exports: [RouterModule],
+  providers: [AdminModule]
 })
 export class AdminModule { }
